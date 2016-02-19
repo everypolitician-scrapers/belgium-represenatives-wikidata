8382dba12374d31acf6819be7d29996aa5da912e
Tony Bowden <tony@tmtm.com>
2016-02-19 09:24:30 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,32 +1,8 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'json'
-require 'pry'
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'mediawiki_api'
 
-def members
-  morph_api_url = 'https://api.morph.io/tmtmtmtm/belgium-represenatives-wikipedia/data.json'
-  morph_api_key = ENV["MORPH_API_KEY"]
-  result = RestClient.get morph_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(wikiname__nl) AS wikiname from data"
-  }
-  JSON.parse(result, symbolize_names: true)
-end
-
-WikiData.ids_from_pages('nl', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
-  puts i if (i % 30).zero?
-  data = WikiData::Fetcher.new(id: p.last).data('nl','fr') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  data[:original_wikiname] = p.first
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/belgium-represenatives-wikipedia', column: 'wikiname__nl')
+EveryPolitician::Wikidata.scrape_wikidata(names: { nl: names }, output: false)
+warn EveryPolitician::Wikidata.notify_rebuilder
