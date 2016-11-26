82f3c73c163eed94281f5317bfccd42cb1b0ca77
Tony Bowden <tony@tmtm.com>
2016-11-26 14:24:15 +0000

    Reverse order of requires to silence warnings

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,9 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'wikidata/fetcher'
 require 'everypolitician'
+require 'wikidata/fetcher'
 
 existing = EveryPolitician::Index.new.country("Belgium").lower_house.popolo.persons.map(&:wikidata).compact
-
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/belgium-represenatives-wikipedia', column: 'wikiname__nl')
 EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { nl: names })
