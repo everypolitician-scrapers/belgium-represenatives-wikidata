3c1ada8ecf9e3f208fdcd0e0d820ec035c845066
Tony Bowden <tony@tmtm.com>
2016-11-26 12:25:02 +0000

    Fetch existing data from EveryPolitician

--- scraper.rb
+++ scraper.rb
@@ -2,6 +2,9 @@
 # encoding: utf-8
 
 require 'wikidata/fetcher'
+require 'everypolitician'
+
+existing = EveryPolitician::Index.new.country("Belgium").lower_house.popolo.persons.map(&:wikidata).compact
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/belgium-represenatives-wikipedia', column: 'wikiname__nl')
-EveryPolitician::Wikidata.scrape_wikidata(names: { nl: names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { nl: names })
