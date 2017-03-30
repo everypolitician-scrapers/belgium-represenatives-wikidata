edf42621fd558a32e9b124c99ac40e4cea2d1e4e
Tony Bowden <tony@tmtm.com>
2017-03-30 11:01:46 +0100

    Also scrape all P39s in the 54th Term

--- scraper.rb
+++ scraper.rb
@@ -7,6 +7,19 @@ require 'wikidata/fetcher'
 existing = EveryPolitician::Index.new.country("Belgium").lower_house.popolo.persons.map(&:wikidata).compact
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'everypolitician-scrapers/belgium-representatives-wikipedia', column: 'wikiname__nl')
 
-ids = %w(Q29051908)
+# Find all P39s of the 54th Term
+query = <<EOS
+  SELECT DISTINCT ?item
+  WHERE
+  {
+    BIND(wd:Q15705021 AS ?membership)
+    BIND(wd:Q17173094 AS ?term)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: existing | ids, names: { nl: names })
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 ?membership .
+    ?position_statement pq:P2937 ?term .
+  }
+EOS
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing | p39s, names: { nl: names })
